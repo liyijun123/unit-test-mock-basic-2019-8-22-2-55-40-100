26fe44eb5fecd50e434fe75e8b55d3c9f18a130d
“liyijun123 <2510243137@qq.com>
2019-08-26 17:41:21 +0800

    modify

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,25 +1,35 @@
 package cashregister;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
 
-
     @Test
+    @Disabled
     public void should_print_the_real_purchase_when_call_process() {
         //given
+     Item[] items = {new Item("test_product", 1)};
+     Purchase purchase = new Purchase(items);
+     MockPrinter mockedPrinter = new MockPrinter();
+     CashRegister cashRegister = new CashRegister(mockedPrinter);
         //when
+     cashRegister.process(purchase);
+   
         //then
+     assertEquals("test_product\t1.0\n", mockedPrinter.gerTempText());
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
         //given
+     StubPurchase stubPurchase = new StubPurchase();
         //when
+     cashRegister.process(stubPurchase);
         //then
+     assertEquals("test_product\\t1.0\n", mockedPrinter.gerTempText());
     }
 
     @Test
